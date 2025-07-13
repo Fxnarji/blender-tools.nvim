c27022432e3626c99ad1c6f228dec8f07fe0830b
Fxnarji <fyluft@stud.mediadesign.de>
2025-07-14 01:47:19 +0200

    more small steps

--- lua/blender-tools/init.lua
+++ lua/blender-tools/init.lua
@@ -4,8 +4,13 @@ local blender_path = "/usr/bin/blender"
 
 function M.setup()
 	vim.api.nvim_create_user_command("BlenderStart", function()
-		print("Hello World!")
-		vim.fn.system("Blender &")
+		print("Hello Wooooorld!")
+		print(vim.inspect(vim.env.PATH))
+		vim.fn.jobstart({ "blender" }, {
+			on_exit = function(_, code)
+				print("Blender exited with code " .. code)
+			end,
+		})
 	end, {})
 end
 return M
